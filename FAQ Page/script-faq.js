07eb084ff8aa51168164146702ea9dd5ed4e6fa0
zhenggbin <S10257745@connect.np.edu.sg>
2023-12-02 18:37:56 +0800

    FAQ Page File

---
+++ FAQ Page/script-faq.js
@@ -0,0 +1,56 @@
+var acc = document.getElementsByClassName("accordion");
+
+// Loop through each accordion element
+for (var i = 0; i < acc.length; i++) {
+  // Add a click event listener to each accordion
+  acc[i].addEventListener("click", function () {
+    
+    // Show and Hide the information
+    var panel = this.nextElementSibling;
+    panel.classList.toggle("pannel-show");
+
+    // Rotate the Chevron Icon when information is showed
+    var chevron = this.querySelector("i.fa");
+    chevron.classList.toggle("rotate");
+  });
+}
+
+
+  // Smooth scroll to top function (defined globally)
+  function scrollingTOP() {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 0
+    });
+  }
+
+  // Wait for the page to finish loading
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.scrolling-animation').forEach(section => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+          }
+        });
+      }, { threshold: 0.2 });
+
+      observer.observe(section);
+    });
+
+  // Show or hide the scroll to top button based on scroll position
+  document.addEventListener("scroll", function () {
+      const scrollToTopBtn = document.getElementById("back-to-top-btn");
+
+      // Check if the button exists in the DOM
+      if (scrollToTopBtn) {
+          // Using if-else loop to check whether the scrolling position is more than 100px from the top of the page
+          if (document.documentElement.scrollTop > 100) {
+              scrollToTopBtn.style.display = "block"; // if true, display the button
+          } else {
+              scrollToTopBtn.style.display = "none"; // if false, hide the button
+          }
+      }
+  });
+});
+
